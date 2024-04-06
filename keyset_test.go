4db46a1c591a803b00a3ce279abf207815d59dd9
Henning Vogt <henning@vogt.family>
2024-04-06 20:17:37 +0200

    Improve code coverage

--- keyset_test.go
+++ keyset_test.go
@@ -9,10 +9,10 @@ var set KeySet
 var rawJSON string = `
 		{
 			"keys": [
-				{ "id": "1", "raw": "1234abcd", "priority": 10 },
 				{ "id": "2", "raw": "1234abce", "priority": 20 },
 				{ "id": "3", "raw": "1234abcf", "priority": 50 },
-				{ "id": "4", "raw": "1234abc0", "priority": 0 }
+				{ "id": "4", "raw": "1234abc0", "priority": 0 },
+				{ "id": "1", "raw": "1234abcd", "priority": 10 }
 			]
 		}`
 
@@ -38,6 +38,12 @@ func TestKeySetFromString(t *testing.T) {
 	if len(set.Keys) != 4 {
 		t.Errorf("Expected 4 keys, got %d", len(set.Keys))
 	}
+
+	set, err = FromString("blob")
+
+	if err == nil {
+		t.Errorf("Expected error, received nil")
+	}
 }
 
 func TestKeySetGetKeyByID(t *testing.T) {
@@ -50,6 +56,24 @@ func TestKeySetGetKeyByID(t *testing.T) {
 	if value.ID != "2" {
 		t.Errorf("Expected key with ID %s, received: %s", "2", value.ID)
 	}
+
+	var emptySet KeySet
+
+	_, err = emptySet.GetKeyByID("2")
+
+	if err == nil {
+		t.Errorf("Expected error, received nil")
+	}
+
+	if err != EmptyKeySetError {
+		t.Errorf("Expected EmptyKeySetError, received: %s", err)
+	}
+
+	_, err = set.GetKeyByID("non-existing")
+
+	if err == nil {
+		t.Errorf("Expected error, received nil")
+	}
 }
 
 func TestKeySetGetKey(t *testing.T) {
@@ -61,5 +85,17 @@ func TestKeySetGetKey(t *testing.T) {
 
 	if value.ID != "1" {
 		t.Errorf("Expected key with ID %s, received: %s", "1", value.ID)
+	}
+
+	var emptySet KeySet
+
+	_, err = emptySet.GetKey()
+
+	if err == nil {
+		t.Errorf("Expected error, received nil")
+	}
+
+	if err != EmptyKeySetError {
+		t.Errorf("Expected EmptyKeySetError, received: %s", err)
 	}
 }
